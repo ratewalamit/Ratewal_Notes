cec89fedd9a3cd0fa52fba8a4cdc31273a44204f
Amit Kumar <ratewalamit@gmail.com>
2024-08-13 21:33:05 +0530

    infor for installing ubuntu

---
+++ linux/after_install_ubuntu.sh
@@ -0,0 +1,8 @@
+#install vim
+#set trackpad settings, Please restart in order to see these settings into effect
+#instll netextender from website #do update java while istallin, see instructions  on website to see how to isntall
+#install chrome
+#install ssh configs
+#change bashrc #from github files
+#install remmina from app center
+#install trashcli
