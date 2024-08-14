9525276ecb16f8f3a857373448b4fefe86d8a795
Amit Kr Ratewal <ratewalamit@gmail.com>
2024-08-14 22:02:20 +0530

    modify for trackpad

--- linux/after_install_ubuntu.sh
+++ linux/after_install_ubuntu.sh
@@ -7,3 +7,29 @@
 #install remmina from app center
 #install trashcli
 #install git
+#install fingerprint 
+#install libtools using: sudo apt install libinput-tools
+#fix trackpad using forlloiwng commands
+Putting these lines in sudo vim /etc/X11/xorg.conf.d/99-synaptics-overrides.conf
+```shell
+Section "InputClass"
+        Identifier "libinput touchpad catchall"
+        MatchIsTouchpad "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "libinput"
+        Option "Tapping" "off"
+        Option "ScrollSpeed" "0.5"
+        Option "PalmDetection" "on"
+        Option "PalmMinWidth" "5"
+        Option "PalmMinZ" "50"
+        Option "ClickMethod" "clickfinger"
+        Option "MiddleClickEnabled" "true"
+        Option "MiddleClickThreshold" "5"
+        Option "PointerAcceleration" "0.5"
+        Option "EdgeScrolling" "on"
+        Option "EdgeScrollLeft" "0"
+        Option "EdgeScrollRight" "0"
+        Option "EdgeScrollUp" "0"
+        Option "EdgeScrollDown" "0"
+EndSection
+```
